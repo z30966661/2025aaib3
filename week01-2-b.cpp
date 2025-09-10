a9ff13f4e9b4ade36d281c3ab4a1da3c5fee8aca
SHYY <pinting63@gmail.com>
2025-09-10 11:45:13 +0800

    week01

---
+++ week01-2-b.cpp
@@ -0,0 +1,5 @@
+#week01-2.py Leetcode 學習計畫 Python版本1 for迴圈
+#Leetcode 28.Find the index of the first occurrence in a string
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)

8ada8c585b3f603fa20b448c80ff80ec32c3e82e
SHYY <pinting63@gmail.com>
2025-11-12 12:03:58 +0800

    Delete week01-2-b.py

--- week01-2-b.py
+++
@@ -1,6 +0,0 @@
-#week01-2.py Leetcode ¾Ç²ß­pµe Pythonª©¥»1 for°j°é
-#Leetcode 28.Find the index of the first occurrence in a string
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
-
